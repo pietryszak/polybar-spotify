ff5cd78dc088c61d664cb2f28859c1b93642864e
Paweł Pietryszak <45850093+pietryszak@users.noreply.github.com>
2022-11-01 12:44:55 +0100

    Change play, pause icons

--- spotify_status.py
+++ spotify_status.py
@@ -69,7 +69,7 @@ def truncate(name, trunclen):
 # Default parameters
 output = fix_string(u'{play_pause} {artist}: {song}')
 trunclen = 35
-play_pause = fix_string(u'\u25B6,\u23F8') # first character is play, second is paused
+play_pause = fix_string(u'\uF8E3,\u25B6')
 
 label_with_font = '%{{T{font}}}{label}%{{T-}}'
 font = args.font
